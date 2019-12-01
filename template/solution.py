444b49462918af9b687e2fce8bd9aec57f70a7df
alfphaderp <36178918+alfphaderp@users.noreply.github.com>
2019-12-01 00:11:02 -0800

    Create template

---
+++ template/solution.py
@@ -0,0 +1,11 @@
+def part1(lines):
+    return 0
+    
+def part2(lines):
+    return 0
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+        lines = f.read().splitlines()
+    print(part1(lines))
+    print(part2(lines))
